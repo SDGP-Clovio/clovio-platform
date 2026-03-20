158e8bf6f87b67dd0942450b97335ef23341586b
rimaz <rimazjr10@gmail.com>
2026-03-21 01:26:08 +0530

    refactor app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import Dashboard from "./pages/Dashboard"
 import { HashRouter, Routes, Route } from "react-router-dom";
 import ProjectTrackingDashboard from "./pages/ProjectTrackingDashboard";
+import SupervisorDashboard from "./pages/SupervisorDashboard";
+import SupervisorProjectsPage from "./pages/SupervisorProjectsPage";
+import SupervisorProjectDetailsPage from "./pages/SupervisorProjectDetailsPage";
 
 
 function App() {
@@ -10,6 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard/>}/>
         <Route path="/project/:id" element={<ProjectTrackingDashboard />}/>
+        <Route path="/supervisor" element={<SupervisorDashboard />} />
+        <Route path="/supervisor/projects" element={<SupervisorProjectsPage />} />
+        <Route path="/supervisor/project/:id" element={<SupervisorProjectDetailsPage />} />
       </Routes>
     </HashRouter>
   )

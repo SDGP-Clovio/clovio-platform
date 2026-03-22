b6a828f84b83465d96e8f22b7b6e90cf403406da
ish80 <ishamhasmin1@gmail.com>
2026-03-22 15:21:27 +0530

    Set username as email in DB

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -4,7 +4,6 @@ from typing import Optional
 # 1. Base properties shared across all user interactions
 class UserBase(BaseModel):
     email: EmailStr
-    username: str
     full_name: Optional[str] = None
 
 # 2. Properties required when a user registers (Create)
